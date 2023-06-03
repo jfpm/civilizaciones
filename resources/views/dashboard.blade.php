a3f387328c0b138bc8742dcdb37db49b86b3363b
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2023-06-03 01:08:06 -0500

    imagen de fondo vista principal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,6 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+<x-app-layout>
+    <div class="py-12" style="background-image: url('/img/fondo_principal.jpg'); background-position: center top; background-repeat: no-repeat; background-size: cover; height:700px">    
+        <center><h1 style="color: white;">DISTRIBUCIÓN DE MODULOS DEL SISTEMA</h1></center>
     </div>
 </x-app-layout>
