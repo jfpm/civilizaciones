06f88681a71c9d67776ffe24c4d101aeb5fa9fe4
StevenDavila <72047032+StevenDavila@users.noreply.github.com>
2023-06-09 22:03:43 -0500

    ajustes dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,50 +1,52 @@
-
 <x-app-layout>
-    
+    <div class="py-12" style="background-image: url('/img/fondo_principal.jpg'); background-position: center top; background-repeat: no-repeat; background-size: cover; height:700px">
+    <center><h1 style="color: white;">CIVILIZACIONES EGIPCIAS</h1></center><br>
+    <center><h2 style="color: white;">Distribución de Modulos del Sistema</h2></center>
     <div class="container">
         <div class="card-container">
-            <div class="card">Tarjeta 1</div>
-            <div class="card">Tarjeta 2</div>
-            <div class="card">Tarjeta 3</div>
+            <div class="card">
+                <br><br>
+                <center>
+                    <button class="round-button"></button>
+                    <br><br>
+                    <label style="color: white;">Visitas:</label>
+                    <br><br><br>
+                    <button class="boton-enlace">Adoraciones </button>
+                </center>
+            </div>
+            <div class="card">
+                <br><br>
+                <center>
+                    <button class="round-button"></button>
+                    <br><br>
+                    <label style="color: white;">Visitas:</label>
+                    <br><br><br>
+                    <button class="boton-enlace">Curiosidades</button>
+                </center>
+            </div>
+            <div class="card">
+                <br><br>
+                <center>
+                    <button class="round-button"></button>
+                    <br><br>
+                    <label style="color: white;">Visitas:</label>
+                    <br><br><br>
+                    <button class="boton-enlace">Cultura General</button>
+                </center>
+            </div>
         </div>
-    </div>      
-    
+    </div>
+    </div>
 </x-app-layout>
+
 <style>
-    .my-div {
-        background-color:#0e0e0779;
-        width: 20%;
-        height: 370px;
-        position: fixed;
-        left: 150px;
-        border-radius: 15px;
-    }
-
-    .my-div-2 {
-        background-color:#0e0e0779;
-        width: 20%;
-        height: 370px;
-        position: fixed;
-        left: 570px;
-        border-radius: 15px;
-    }
-
-    .my-div-3 {
-        background-color:#0e0e0779;
-        width: 20%;
-        height: 370px;
-        position: fixed;
-        left: 1000px;
-        border-radius: 15px;
-    }
-
     .round-button {
         border: none;
         border-radius: 45%;
         background-color: #e3e8eeb2;
         padding: 30px 30px;
         font-size: 16px;
-        cursor: pointer;  
+        cursor: pointer;
     }
 
     .boton-enlace {
@@ -53,7 +55,7 @@
         background-color: #e3e8eeb2;
         padding: 20px 70px;
         font-size: 16px;
-        cursor: pointer; 
+        cursor: pointer;
     }
 
     .row {
@@ -65,24 +67,42 @@
     }
 
     .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-}
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 75vh;
+    }
 
-.card-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-}
+    .card-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 
-.card {
-    width: 300px;
-    height: 200px;
-    margin: 10px;
-    background-color: #e3e3e3;
-}
+    .card {
+        width: 250px;
+        height: 350px;
+        margin: 40px;
+        background-color: #0e0e0779;
+        border-radius: 15px;
+        
+    }
 
+    /* Media query para dispositivos móviles */
+    @media only screen and (max-width: 600px) {
+        .container {
+            flex-direction: column;
+            align-items: stretch;
+        }
 
-</style>    
+        .my-div,
+        .my-div-2,
+        .my-div-3 {
+            position: static;
+            width: auto;
+            height: auto;
+            margin-top: 20px;
+            left: auto;
+        }
+    }
+</style>
