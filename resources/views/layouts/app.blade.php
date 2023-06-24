9afc286a0977610b009350f770c6dcf45735a7c8
Jhon Freddy Popo Moreno <jhonfreddypopomoreno@gmail.com>
2023-06-23 23:43:37 -0500

    cambios organizados

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,7 +82,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-200">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
